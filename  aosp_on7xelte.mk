776da694a13e9b6039ba5311a4a0b6628adbb1d2
DarkLord1731 <darklord1731@gmail.com>
2018-02-03 14:20:31 +0530

    on7xelte: Initialise AEX 8.1

---  aosp_on7xelte.mk
+++  aosp_on7xelte.mk
@@ -1,11 +1,34 @@
-$(call inherit-product, device/samsung/on7xelte/full_on7xelte.mk)
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Inherit some common aosp stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
-# Use Magisk
-DEFAULT_ROOT_METHOD := magisk
+## Device identifier. This must come after all inclusions
+# Inherit device configuration
+$(call inherit-product, device/samsung/on7xelte/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_on7xelte
+PRODUCT_DEVICE := on7xelte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G610F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_RELEASE_NAME := SM-G610F
